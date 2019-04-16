2c278d9fe8c2d0ceb8485ef5b7e354b1c0af5be1
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-04-16 15:29:23 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fileupload = require('express-fileupload')
+
+
+const app = express();
+
+
+app.use(fileupload());
+
+//upload Endpoint
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: 'No file has been uploaded.'})
+    }
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
+    });
+})
+
+
+
+app.listen(5000, () => console.log('server started'))
